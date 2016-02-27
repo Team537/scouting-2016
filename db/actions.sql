4b4876be94d7167d6dd46d88da79ec615db2ea93
Five Three Seven Scouter <Five Three Seven Scouter>
2016-02-27 09:49:03 -0600

    initial db

---
+++ db/actions.sql
@@ -0,0 +1,23 @@
+#TRUNCATE TABLE `scouting2016`.`actions`;
+
+INSERT INTO `scouting2016`.`actions`
+(`action_name`,
+`points`)
+VALUES
+('Start', 0),
+('High Goal', 5),
+('Low Goal', 2),
+('Reach', 5),
+('Boulder Pickup', 0),
+('Cross Portcullis (A)', 5),
+('Cross Cheval de Frise (A)', 5),
+('Cross Moat (B)', 5),
+('Cross Ramparts (B)', 5),
+('Cross Drawbridge (C)', 5),
+('Cross Sally Port (C)', 5),
+('Cross Rock Wall (D)', 5),
+('Cross Rough Terrain (D)', 5),
+('Cross Low Bar', 5),
+('Challange', 5),
+('Scale', 15)
+;
---
+++ db/locations.sql
@@ -0,0 +1,23 @@
+INSERT INTO `scouting2016`.`locations`
+(`location_name`)
+VALUES
+('Own Secret Passage'),
+('Opponent Secret Passage'),
+('Own Courtyard'),
+('Own Defense 1'),
+('Own Defense 2'),
+('Own Defense 3'),
+('Own Defense 4'),
+('Own Defense 5'),
+('Opponent Defense 1'),
+('Opponent Defense 2'),
+('Opponent Defense 3'),
+('Opponent Defense 4'),
+('Opponent Defense 5'),
+('Brattice'),
+('Neutral Zone Left'),
+('Neutral Zone Right'),
+('Tower Right'),
+('Tower Center'),
+('Tower Left')
+;
